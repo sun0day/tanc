7c2bc5b4255dde73cfc7ae7f6c7c00232924e3a7
sun0day <sun_day500@163.com>
2025-04-14 11:32:40 +0800

    chore: build and test in qemu docker

--- qemu.sh
+++ qemu.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt upgrade
-sudo apt install cmake
+apt update && apt install -y cmake
 
 mkdir build 
 cd build 
 cmake .. -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_BUILD_TYPE=$BUILD_TYPE 
+
 cd .. 
 cmake --build ./build --config $BUILD_TYPE
-file ./build/test/test
+
 ./build/test/test
