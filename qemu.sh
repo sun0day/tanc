d23f40ef3dd2323bda3fd8b7ffa0d227601acc05
sun0day <sun_day500@163.com>
2025-04-14 12:11:49 +0800

    chore: run workflow in qemu docker  (#4)
    * chore: pr workflow trigger files
    
    * chore: build and test in qemu docker

---
+++ qemu.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+apt update && apt install -y cmake g++
+
+mkdir build 
+cd build 
+cmake .. -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_BUILD_TYPE=$BUILD_TYPE 
+
+cd .. 
+cmake --build ./build --config $BUILD_TYPE
+
+ls -lh ./build
+
+./build/test/test
