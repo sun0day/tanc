123021d674ccdf76937b3f71428bd20d15d0d707
sun0day <sun_day500@163.com>
2025-05-04 15:51:27 +0800

    chore: no chmod in windows latest

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ case "$(uname -s)" in
     *)          export TEST_BIN="" TEST_SUFFIX="" ;;
 esac
 
-cmd /c "$TEST_ROOT/list$TEST_BIN/list_test$TEST_SUFFIX"
+cmd /c $TEST_ROOT/list$TEST_BIN/list_test$TEST_SUFFIX
 
 
 "$TEST_ROOT/list$TEST_BIN/list_test$TEST_SUFFIX"
