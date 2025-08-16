eb198c73739126274b1f81941bc9b93744aef5b2
sun0day <sun_day500@163.com>
2025-08-16 16:40:24 +0800

    style(print): print.c format

--- lib/print/print.c
+++ lib/print/print.c
@@ -1,6 +1,8 @@
 #include "print.h"
+
 #include <stdio.h>
 #include <string.h>
+
 #include "common.h"
 
 char *_tc_stdio_buf = NULL;
@@ -13,9 +15,9 @@ void _tc_print_set_buf(size_t size, _tc_malloc_ptr malloc, _tc_free_ptr free) {
   _tc_stdio_len = 0;
   _tc_stdio_thres = 7 * size / 8;
 
-  if(_tc_stdio_buf) {
+  if (_tc_stdio_buf) {
     free(_tc_stdio_buf);
   }
-  
+
   _tc_stdio_buf = (char *)malloc(size);
 }
